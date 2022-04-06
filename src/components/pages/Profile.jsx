703139576e6c4ef7a226fefdaea8523d0e27ac61
Ryosei <64691162+137ryo@users.noreply.github.com>
2022-04-06 18:27:41 +0900

    スキル・プロフィール大枠作成

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,7 +1,29 @@
 export const Profile = () => {
   return (
     <div>
-      <p>hello</p>
+      <h2>プロフィール</h2>
+      <div>
+        <h3>自己紹介</h3>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+      <hr />
+      <div>
+        <h3>経歴</h3>
+        <ul>
+          <li>東京電子専門学校　高度情報システム科4年制卒</li>
+          <li>IT企業のソリューション事業部に所属</li>
+        </ul>
+      </div>
+      <hr />
+      <div>
+        <h3>取得資格</h3>
+        <ul>
+          <li>情報セキュリティマネジメント試験</li>
+          <li>応用情報技術者試験</li>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/components/pages/Skill.jsx
+++ src/components/pages/Skill.jsx
@@ -1,7 +1,65 @@
 export const Skill = () => {
   return (
     <div>
-      <p>hello</p>
+      <h2>スキル</h2>
+      <hr/>
+      <div>
+        <h3>言語</h3>
+        <ul>
+          <li>Java</li>
+          <li>C</li>
+          <li>PHP</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JSP</li>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>TypeScript</li>
+          <li>Node.js</li>
+          <li>Python</li>
+          <li>CShell</li>
+        </ul>
+      </div>
+      <hr/>
+      <div>
+        <h3>フレームワーク</h3>
+        <ul>
+          <li>Spring</li>
+          <li>Struts</li>
+          <li>Django</li>
+          <li>Flask</li>
+          <li>BootStrap</li>
+          <li>Pega</li>
+        </ul>
+      </div>
+      <hr/>
+      <div>
+        <h3>ツールなど</h3>
+        <ul>
+          <li>TeraTarm</li>
+          <li>WinSCP</li>
+          <li>Postman</li>
+          <li>Eclipse</li>
+          <li>Visual Studio Code</li>
+          <li>Sakura Editor</li>
+          <li>SQL Developer</li>
+          <li>A5M2</li>
+          <li>WinMarge</li>
+          <li>Github</li>
+          <li>SVN Repository</li>
+          <li>Microsoft Teams</li>
+          <li>Slack</li>
+          <li>Zoom</li>
+        </ul>
+      </div>
+      <hr/>
+      <div>
+        <h3>その他</h3>
+        <ul>
+          <li>XML</li>
+          <li>Json</li>
+        </ul>
+      </div>
     </div>
   );
 };
