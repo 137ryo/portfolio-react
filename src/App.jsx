be41c965ef0f8fcf0738d081ec3a989a37db03fc
Ryosei <64691162+137ryo@users.noreply.github.com>
2022-05-05 13:35:06 +0900

    stop

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
+import Header from "./components/molecules/Header";
 import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Router />
     </BrowserRouter>
   );
